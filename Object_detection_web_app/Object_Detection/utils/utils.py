b04ea40d56627698736c17cd0f20d6143a572bf6
milad-s5 <82322980+milad-s5@users.noreply.github.com>
2022-01-30 11:09:33 +0330

    Add files via upload

---
+++ Object_detection_web_app/Object_Detection/utils/utils.py
@@ -0,0 +1,73 @@
+import time
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def return_boxes(net, image, iou_thresh, nms_thresh, LABELS):
+
+    # initialize a list of colors to represent each possible class label
+    np.random.seed(42)
+    COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
+                          
+    (H, W) = image.shape[:2]
+
+    ln = net.getLayerNames()
+    ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
+
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    start = time.time()
+    layerOutputs = net.forward(ln)
+    end = time.time()
+    # show timing information on YOLO
+    detection_time = end - start
+
+
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    # loop over each of the layer outputs
+    for output in layerOutputs:
+        # loop over each of the detections
+        for detection in output:
+
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+
+            if confidence > iou_thresh:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, iou_thresh, nms_thresh)
+
+
+    if len(idxs) > 0:
+
+        for i in idxs.flatten():
+
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+
+            color = [int(c) for c in COLORS[classIDs[i]]]
+            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+            text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+            cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    #cv2.imshow("Image", image)
+
+    _, t = plt.subplots(1, 1)
+    t.imshow(image)
+    plt.axis('off')
+    plt.savefig("Object_Detection/static/test.jpeg", dpi=360, bbox_inches='tight')
+
+    return detection_time
---
+++ Object_detection_web_app/Object_Detection/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+import io
+from PIL import Image
+import cv2
+import numpy as np
+from base64 import b64decode
+from .utils import *
+
+
+@csrf_exempt
+def object_detection_api(api_request):
+    json_object = {'success': False}
+
+    if api_request.method == "POST":
+
+        if api_request.POST.get("image64", None) is not None:
+            base64_data = api_request.POST.get("image64", None).split(',', 1)[1]
+            data = b64decode(base64_data)
+            data = np.array(Image.open(io.BytesIO(data)))
+            detection_time = detection(data)
+
+        elif api_request.FILES.get("image", None) is not None:
+            image_api_request = api_request.FILES["image"]
+            image_bytes = image_api_request.read()
+            image = Image.open(io.BytesIO(image_bytes))
+            detection_time = detection(image)
+
+    json_object['success'] = True
+    json_object['time'] = str(round(detection_time))+" seconds"
+    print(json_object)
+    return JsonResponse(json_object)
+
+
+def detect_request(api_request):
+    return render(api_request, 'index.html')
+
+
+def detection(original_image):
+    cfg_file = './yolov3.cfg'
+    weight_file = './yolov3.weights'
+    names = './coco.names'
+
+    m = cv2.dnn.readNetFromDarknet(cfg_file, weight_file)
+    class_names = open(names).read().strip().split("\n")
+
+    nms_thresh = 0.6
+    iou_thresh = 0.4
+
+    detection_time = return_boxes(m, original_image, iou_thresh, nms_thresh, class_names)
+
+    return detection_time
